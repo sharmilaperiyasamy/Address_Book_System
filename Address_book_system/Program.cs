56b6e22c0ac49235f7df15c349404bc13b257bc4
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-08 14:22:01 +0530

    [Sharmila] edit contact using name

--- Address_book_system/Program.cs
+++ Address_book_system/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Address_book_system;
-internal class Directory
+internal class program
 {
     public static List<Person_Details> details = new List<Person_Details>();
 
     public static void Main(String[] args)
     {
         Address_book_system.person.createNewContact();
+        Address_book_system.person.displayContacts();
+        Address_book_system.person.editContact();
+        Address_book_system.person.displayContacts();
     }
 }
