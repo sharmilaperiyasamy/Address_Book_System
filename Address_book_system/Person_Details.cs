f14f22237d38af5cff7bb3b797ce785278c504f1
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-08 13:12:24 +0530

    [Sharmila] create contact

---
+++ Address_book_system/Person_Details.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_book_system
+{
+    internal class Person_Details
+    {
+        public string first_Name { get; set; }
+        public string last_Name { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public string phone_Number { get; set; }
+        public string email { get; set; }
+
+
+    }
+}
